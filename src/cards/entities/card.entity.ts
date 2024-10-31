29bd9c20e393a8122b449ff5aea48cb16edca0e4
Your Name <you@example.com>
2024-10-31 15:30:36 -0300

    Criando entidade de cards

--- src/cards/entities/card.entity.ts
+++ src/cards/entities/card.entity.ts
@@ -1,1 +1,21 @@
-export class Card {}
+import { Material } from '../../material/entities/material.entity';
+
+export class Card {
+
+id: number;
+title: String;
+description: String;
+img_url: String;
+hidden: Boolean;
+subtitle: String;
+material: Material[];
+
+constructor(id: number, title: String, description: String,img_url: String,hidden: Boolean,subtitle: String,material: Material[]){
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.img_url = img_url;
+    this.material = material;
+    
+}
+}
