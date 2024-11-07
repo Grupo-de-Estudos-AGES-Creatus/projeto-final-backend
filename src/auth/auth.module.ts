03efe45c2a710498fcca4bdf1846d049902b1a7e
Luan Pacheco Lima <luanlimapacheco@gmail.com>
2024-11-07 16:26:30 -0300

    ajustando os exports e providors

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,22 +1,22 @@
-
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UserModule } from '../user/user.module';
+import { UserModule } from '../user/user.module'; 
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { jwtConstants } from './constants';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
-    UserModule,
+    UserModule, 
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '60s' },
     }),
   ],
-  providers: [AuthService,JwtModule],
+  providers: [AuthService, AuthGuard],  
   controllers: [AuthController],
-  exports: [AuthService],
+  exports: [AuthService, AuthGuard], 
 })
 export class AuthModule {}
--- src/cards/cards.module.ts
+++ src/cards/cards.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CardsService } from './cards.service';
 import { CardsController } from './cards.controller';
+import { AuthModule } from '../auth/auth.module'; 
 
 @Module({
+  imports: [AuthModule],  
   controllers: [CardsController],
   providers: [CardsService],
 })
