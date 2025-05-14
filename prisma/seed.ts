f8fe5205b8a8d9b3d7370e77eae9630e00979df5
hmpuc <henrique.mairesse@edu.pucrs.br>
2025-05-14 14:01:35 -0300

    14.05

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,22 +4,18 @@ const prisma = new PrismaClient()
 
 async function main() {
     const user: User = {
-        id: 1,
+        id: 0,
         email: 'leo@gmail.com',
         username: 'Leo',
         password: 'coco123',
         role: 'ADMIN',
-        matricula: "23200064",
-        course: "ES",
-        github: "github.com/leo",
-        semester: "242",
-        n_of_absences: 1,
+        registration: "23200064",
+        semester: "2025/1",
         img_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        created_at: new Date()
+        created_at: new Date();
     }
     const alice = await prisma.user.create({
         data: user
-    })
-
+    })
 }
 main();
