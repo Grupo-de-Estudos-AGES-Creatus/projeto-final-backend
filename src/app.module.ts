175dca86405a8df5859b8067af166f66dd5783c1
hmpuc <henrique.mairesse@edu.pucrs.br>
2025-05-15 15:39:56 -0300

    15.05

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { EventModule } from './event/event.module';
 import { MaterialModule } from './material/material.module';
-import { CalendarController } from './calendar/calendar.controller';
 import { CalendarModule } from './calendar/calendar.module';
 import { PrismaService } from './prisma.service';
-import { CalendarService } from './calendar/calendar.service';
 import { ProjectModule } from './project/project.module';
-import { MaterialModule } from './material/material.module';
 
 @Module({
-  imports: [UserModule, EventModule, MaterialModule, CalendarModule, ProjectModule],
+  imports: [UserModule, MaterialModule, CalendarModule, ProjectModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
