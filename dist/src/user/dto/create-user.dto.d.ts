ca251a1e4dfe72633377caf4f864d7793236ae91
Henrique Collatto <chcollatto@gmail.com>
2025-05-16 14:23:56 -0300

    ..

--- dist/src/user/dto/create-user.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateUserDto {
-    email: string;
-    username: string;
-    password: string;
-    role: string;
-    registration: string;
-    semester: string;
-    imgPath?: string;
-}
