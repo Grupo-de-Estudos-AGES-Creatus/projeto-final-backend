2aefc3397689d825d255ab7831e9ef4729d32d3f
Henrique Collatto <chcollatto@gmail.com>
2025-05-21 15:36:57 -0300

    material with swagger

--- src/material/material.controller.ts
+++ src/material/material.controller.ts
@@ -2,42 +2,63 @@ import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from 
 import { MaterialService } from './material.service';
 import { CreateMaterial } from './dto/material.dto';
 import { UpdateMaterial } from './dto/update-material.dto';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+
 @Controller('material')
 export class MaterialController {
     constructor(private materialService: MaterialService) {}
         
     // Pega todos os materiais
     @Get()
+    @ApiOperation({ summary: 'Get all materials' })
+    @ApiResponse({ status: 200, description: 'List of materials.' })
     async getAll() {
         return this.materialService.getAll();
     }
     
     // Pega um material pelo id
     @Get(':id')
+    @ApiOperation({ summary: 'Get material by id' })
+    @ApiResponse({ status: 200, description: 'Material found.' })
+    @ApiResponse({ status: 404, description: 'Material not found.' })
     async getOne(@Param('id', ParseIntPipe) id: number) {
         return this.materialService.getOne(id);
     }
 
     // Pega os materiais de uma sprint usando o id da sprint
     @Get('/sprint/:id')
+    @ApiOperation({ summary: 'Get all materials of the sprint by id' })
+    @ApiResponse({ status: 200, description: 'Sprint found.' })
+    @ApiResponse({ status: 404, description: 'Sprint not found.' })
     async getOneBySprint(@Param('id', ParseIntPipe) sprintId: number) {
         return this.materialService.getOneBySprint(sprintId);
     }
     
     // Cria um material
     @Post()
+    @ApiOperation({ summary: 'Create a new material' })
+    @ApiResponse({ status: 201, description: 'Material created successfully.' })
+    @ApiResponse({ status: 400, description: 'Bad request.' })
+    @ApiBody({ type: CreateMaterial })
     async create(@Body() material: CreateMaterial) {
         return this.materialService.create(material);
     }
 
     // Atualiza um material
     @Patch(':id')
+    @ApiOperation({ summary: 'Update material by id' })
+    @ApiResponse({ status: 200, description: 'Material updated successfully.' })
+    @ApiResponse({ status: 404, description: 'Material not found.' })
+    @ApiBody({ type: UpdateMaterial })
     async update(@Param('id', ParseIntPipe) id: number, @Body() material: UpdateMaterial) {
         return this.materialService.update(id, material);
     }
 
     // Deleta um material
     @Delete(':id')
+    @ApiOperation({ summary: 'Delete material by id' })
+    @ApiResponse({ status: 200, description: 'Material deleted successfully.' })
+    @ApiResponse({ status: 404, description: 'Material not found.' })
     async delete(@Param('id', ParseIntPipe) id: number) {
         return this.materialService.delete(id);
     }
