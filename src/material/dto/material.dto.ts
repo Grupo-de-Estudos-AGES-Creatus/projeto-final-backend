09a5ba8d5d6a72f41c5946055f763ab933e4d0e2
Henrique Collatto <chcollatto@gmail.com>
2025-05-21 15:24:29 -0300

    create material dto

--- src/material/dto/material.dto.ts
+++ src/material/dto/material.dto.ts
@@ -1,18 +1,31 @@
 import { Optional } from '@nestjs/common'
+import { ApiProperty } from '@nestjs/swagger'
 import { IsInt, IsNotEmpty, IsString } from 'class-validator'
 
 
 // Filtro para criar um material
 export class CreateMaterial {
 
+    @ApiProperty({
+    description: 'The ID of the sprint the material belongs to',
+    example: 2,
+    })
     @IsInt()
     sprintId: number
 
-    @IsNotEmpty()
-    @IsString()
-    text: string
-
+    @ApiProperty({
+    description: 'Material title',
+    example: 'Programming content',
+    })
     @IsNotEmpty()
     @IsString()
     title: string
+
+    @ApiProperty({
+    description: 'Material description',
+    example: 'Programming content',
+    })
+    @IsNotEmpty()
+    @IsString()
+    text: string
 }
