0f3a7a256f7e88a0bb9bdb6947d982368a0a2203
Fernando A <mcflyj038@gmail.com>
2025-05-20 23:41:22 -0300

    .

--- src/user/dto/verify-user.dto.ts
+++ src/user/dto/verify-user.dto.ts
@@ -1,9 +1,19 @@
 import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export class VerifyUserDto {
+    @ApiPropertyOptional({
+        description: "User email",
+        example: "john.doe@unknown.com"
+    })
     @IsNotEmpty()
     @IsString()
     email: string;
     
+    @ApiPropertyOptional({
+        description: "User password",
+        example: "1234"
+    })
     @IsNotEmpty()
     @IsString()
     password: string;
