f8aa2a7eb17f9965171890279c87632e5f1997f8
Henrique Collatto <chcollatto@gmail.com>
2025-05-21 15:54:21 -0300

    sprint dto 1

--- src/sprint/dto/create-sprint.dto.ts
+++ src/sprint/dto/create-sprint.dto.ts
@@ -1,16 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsBoolean } from 'class-validator';
 
 
 export class CreateSprintDto{
 
+    @ApiProperty({
+        description: 'Sprint title',
+        example: 'Sprint 5 - Portfolio',
+      })
     @IsNotEmpty()
     @IsString()
     title: string;
 
+
+    @ApiProperty({
+        description: 'Sprint description',
+        example: 'Make a portfolio',
+      })
     @IsNotEmpty()
     @IsString()
     descriptionPath: string;
 
+    @ApiProperty({
+        description: 'This sprint is locked?',
+        example: true,
+      })
     @IsBoolean()
     isLocked: boolean;
     
