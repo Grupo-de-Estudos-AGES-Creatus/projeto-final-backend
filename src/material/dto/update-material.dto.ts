eb5851c96d425344cf99d991d765dbc385506ac8
Fernando A <mcflyj038@gmail.com>
2025-05-21 00:26:40 -0300

    documentacao updateMaterial

--- src/material/dto/update-material.dto.ts
+++ src/material/dto/update-material.dto.ts
@@ -1,14 +1,24 @@
 import { Optional } from "@nestjs/common"
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsString } from "class-validator"
 
 // Filtro para atualizar um material
 export class UpdateMaterial {
 
+    @ApiProperty({
+        description: "Update material text",
+        example: "Medicine content"
+    })
     @Optional()
     @IsNotEmpty()
     @IsString()
     text: string
 
+
+    @ApiProperty({
+        description: "Update material title",
+        example: "Medicine"
+    })
     @Optional()
     @IsNotEmpty()
     @IsString()
