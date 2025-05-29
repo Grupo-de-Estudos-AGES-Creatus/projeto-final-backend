b679bc1aac961471e0e13e5496b763f6c26215a8
hmpuc <henrique.mairesse@edu.pucrs.br>
2025-05-29 15:07:02 -0300

    Calendar date fix

--- src/calendar/dto/update-calendarEvent.dto.ts
+++ src/calendar/dto/update-calendarEvent.dto.ts
@@ -1,28 +1,28 @@
-import { IsDate, IsNotEmpty, IsString, IsOptional } from 'class-validator'
+import { IsNotEmpty, IsString, IsOptional, IsDateString } from 'class-validator'
 import { ApiPropertyOptional } from "@nestjs/swagger"
 
 // Filtro para editar um evento no calendário
 export class UpdateCalendarEventDto {
 
-    @ApiPropertyOptional({ description: "Calendar event title" })
+    @ApiPropertyOptional({ description: "Calendar event title", example: "Sprint 1 end date" })
     @IsOptional()
     @IsNotEmpty()
     @IsString()
     title: string
 
-    @ApiPropertyOptional({ description: "Calendar event description" })
+    @ApiPropertyOptional({ description: "Calendar event description", example: "End date to submit your project!" })
     @IsOptional()
     @IsNotEmpty()
     @IsString()
     description: string
 
-    @ApiPropertyOptional({ description: "Start date of the calendar event" })
+    @ApiPropertyOptional({ description: "Start date of the calendar event", example: new Date() })
     @IsOptional()
-    @IsDate()
-    startDate: Date
+    @IsDateString()
+    startDate: string
     
-    @ApiPropertyOptional({ description: "End date of the calendar event" })
+    @ApiPropertyOptional({ description: "End date of the calendar event", example: new Date() })
     @IsOptional()
-    @IsDate()
-    endDate: Date
+    @IsDateString()
+    endDate: string
 } 
